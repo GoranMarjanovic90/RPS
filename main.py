1c40c0f0330d6a54540a4975af511e7b2bcafd43
GoranMarjanovic90 <goranmarjanovic1990@gmail.com>
2022-10-08 14:08:11 +0200

    Commitment

---
+++ main.py
@@ -0,0 +1,32 @@
+import random
+
+def play():
+    player = ""
+
+    print("Welcome to the game of rock paper scissors!")
+    print("Exit the game? - press e")
+    
+    while player != 'e':
+        player = input("Press r for rock, p for paper, s for scissors. What is your choice?")
+        if player == 'e':
+            exit()
+        pc = random.choice(["r", "p", "s"])
+        print(f"I choose {pc}")
+        if player == pc:
+            return "no winner"
+        if player_win(player, pc):
+            return "You won!"
+        else:
+            return "I won!"
+    
+def player_win(player, pc):
+    if(player == 'r' and pc == 's') or (player == 's' and pc == 'p')or(player == 'p' and pc == 'r'):
+        return True
+
+
+print(play())
+while player_win:
+    print(play())
+   
+
+
---
+++ readme.md
@@ -0,0 +1,4 @@
+Welcome to The game of rock paper scissors!
+
+Rock wins against scissors; paper wins against rock; and scissors wins against paper! If both players throw the same
+hand signal, it is considered a tie, and play resumes until there is a clear winner.
